80f7ec7c57429248271ee69bd7f9ef1a1221f52a
palashpatel15 <palash.patel@medscred.com>
2026-09-23 18:51:53 +0530

    ok

--- run_vllm.sh
+++ run_vllm.sh
@@ -2,4 +2,4 @@ nohup vllm serve zai-org/GLM-OCR \
     --port 8081 \
     --served-model-name glm-ocr \
     --gpu-memory-utilization 0.70 \
-    --max-model-len 32768 > > vllm.log 2>&1 &
+    --max-model-len 32768 > vllm.log 2>&1 &
